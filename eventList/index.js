69531cf818d23e341b2aa12e4b065710e722be96
AureAle <aperez33@ucol.mx>
2019-07-22 18:06:40 -0500

    events

--- eventList/index.js
+++ eventList/index.js
@@ -5,7 +5,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 
 // Create a json-server with our JSON file on disk
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('events.json');
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
